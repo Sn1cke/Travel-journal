96e63cd38c9ada2184d3ad1fddc40ff899be7496
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-02-22 20:44:27 +0100

    Fix body bg color and map links

--- src/data.tsx
+++ src/data.tsx
@@ -1,37 +1,38 @@
 export default [
   {
     id: 1,
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-  },
-  {
-    id: 2,
-    title: "Sydney Opera House",
-    location: "Australia",
-    googleMapsUrl: "https://goo.gl/maps/jAcyr7EkuvtBMsRw5",
-    startDate: "01 Jan, 2021",
-    endDate: "14 Jan, 2021",
-    description:
-      "The Sydney Opera House is a multi-venue performing arts centre located at Sydney Harbour. It is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th-century architecture.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-  },
-  {
-    id: 3,
     title: "Königsee",
     location: "Germany",
-    googleMapsUrl: "https://goo.gl/maps/SHYh9ZAUKkgdNeuw8",
-    startDate: "22 Jan, 2022",
-    endDate: "24 Jan, 2023",
+    googleMapsUrl: "https://goo.gl/maps/nMiuPsr3fxoJKDQJ6",
+    startDate: "22 Jun, 2019",
+    endDate: "30 Jun, 2019",
     description:
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. It is a 15-kilometre-long branch off the Sunnylvsfjorden.",
     imageUrl:
       "https://images.unsplash.com/photo-1672606853233-8beef07fe711?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  },
+  {
+    id: 2,
+    title: "Dallas, Texas",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/zVS6Ng4jYZcasFHC6",
+    startDate: "12 Oct, 2019",
+    endDate: "10 Dec, 2019",
+    description:
+      "Dallas is a commercial and cultural hub of the region. In the Arts District, the Dallas Museum of Art and the Crow Collection of Asian Art cover thousands of years of art.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1613202930807-a2cc5bd86702?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  },
+  {
+    id: 3,
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/jAcyr7EkuvtBMsRw5",
+    startDate: "01 Mar, 2020",
+    endDate: "14 Aug, 2020",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre located at Sydney Harbour. It is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th-century architecture.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   },
 ];
